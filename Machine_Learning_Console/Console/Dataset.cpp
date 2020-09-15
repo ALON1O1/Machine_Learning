84c8cb5c3d17532504e2346916dd77ed41d4cbe1
ALON1O1 <alon.armoni@gmail.com>
2020-09-15 18:50:39 +0300

    Dataset class finished, not tested

--- Machine_Learning_Console/Console/Dataset.cpp
+++ Machine_Learning_Console/Console/Dataset.cpp
@@ -1,4 +1,4 @@
-#include "Dataset.h"
+//#include "Dataset.h"
 #include <vector>
 #include <string>
 
@@ -6,40 +6,52 @@ using namespace std;
 class Dataset {
 
 	private:
-		vector<float[]> inputs = vector<float[]>();
-		vector<float[]> results = vector<float[]>();
+		vector<float*> inputs = vector<float*>();
+		vector<float*> results = vector<float*>();
 		int length = 0;
 
 	public:
 		Dataset() {
 			
 		}
-
 		int getLength() {
 			return length;
 		}
 		void addData(float inputs[], float results[]) {
-
-		}
-		void getData(vector<float[]> inputs, vector<float[]> results) {
-
+			this->inputs.push_back(inputs);
+			this->results.push_back(results);
 		}
 		float* getInputs(int index) {
-
+			static float* returnValue = inputs[index];
+			return returnValue;
 		}
-		vector<float[]> getAllInputs() {
+		vector<float*> getAllInputs() {
 			return inputs;
 		}
-		vector<float[]> getAllResults() {
+		vector<float*> getAllResults() {
 			return results;
 		}
 		float* getResults(int index) {
-
+			static float* returnValue = results[index];
+			return returnValue;
 		}
-		string* getSaveString() {
-
+		vector<string> getSaveString() {
+			vector<string> returnValues = vector<string>();
+			for (int i = 0; i < (int)(inputs.size()); i++) {
+				string s = inputs.size() + "";
+				for (int j = 0; j < sizeof(inputs[i]); j++) {
+					s += "," + to_string(inputs.at(i)[j]);
+				}
+				s += "," + results.size();
+				for (int j = 0; j < sizeof(results[i]); j++) {
+					s += "," + to_string(inputs.at(i)[j]);
+				}
+				returnValues.push_back(s);
+			}
+			return returnValues;
 		}
 		void removeData(int index) {
-
+			inputs.erase(inputs.begin() + index);
+			results.erase(results.begin() + index);
 		}
 };
--- Machine_Learning_Console/Console/Dataset.h
+++ Machine_Learning_Console/Console/Dataset.h
@@ -5,12 +5,11 @@ class Dataset
 		Dataset();
 		int getLength();
 		void addData(float inputs[], float results[]);
-		void getData(vector<float[]> inputs, vector<float[]> results);
 		float* getInputs(int index);
-		vector<float[]>getAllInputs();
-		vector<float[]>getAllResults();
+		vector<float*>getAllInputs();
+		vector<float*>getAllResults();
 		float* getResults(int index);
-		string* getSaveString();
+		vector<string> getSaveString();
 		void removeData(int index);
 };
 
