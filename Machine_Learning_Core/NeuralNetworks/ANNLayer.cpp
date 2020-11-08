acf07648e6fd7cb278e94bd6cdd8c3e2192c7554
ALON1O1 <alon.armoni@gmail.com>
2020-11-09 00:50:48 +0200

    ANNLayer derive function done

--- Machine_Learning_Core/NeuralNetworks/ANNLayer.cpp
+++ Machine_Learning_Core/NeuralNetworks/ANNLayer.cpp
@@ -30,5 +30,23 @@ namespace NeuralNetwork {
 			default: return false;
 		}	
 	}
+	bool ANNLayer::derive(std::vector<float> values, std::vector<float>& return_values) {
+		switch (function) {
+			case ActivationFunction::identity: for (float v : values) return_values.push_back(1); return true;
+			case ActivationFunction::sigmoid:
+				for (float v : values) {
+					float sigmoid = 1 / (1 + pow(M_E, -v));
+					return_values.push_back(sigmoid * (1 - sigmoid));
+				}
+				return true;
+			case ActivationFunction::softmax:
+				activate(values, return_values);
+				for (int i = 0; i < return_values.size(); i++) {
+					return_values[i] = return_values[i] * (1 - return_values[i]);
+				}
+				return true;
+			default: return false;
+		}
+	}
 
 }
