3cb1a1a2f1f7faf1a0ac4573ad1cd9d2aaaa7526
ALON1O1 <alon.armoni@gmail.com>
2020-11-10 00:21:18 +0200

    NetworksList class done, not tested

---
+++ Machine_Learning_Console/Console/NetworkList.cpp
@@ -0,0 +1,46 @@
+#include "NetworkList.h"
+
+NetworkList::NetworkList() {
+	names = std::vector<std::string>();
+	networks = std::vector<NeuralNetwork::NeuralNetwork>();
+	length = 0;
+}
+
+bool NetworkList::addNetwork(NeuralNetwork::NeuralNetwork network, std::string name) {
+	if (getNetworkIndex(name) != -1) return false;
+	networks.push_back(network);
+	names.push_back(name);
+	length++;
+	return true;
+}
+bool NetworkList::removeNetwork(std::string name) {
+	int index = getNetworkIndex(name);
+	if (index == -1) return false;
+	names.erase(names.begin() + index);
+	networks.erase(networks.begin() + index);
+	length--;
+	return true;
+}
+void NetworkList::removeAll() {
+	networks.clear();
+	length = 0;
+}
+
+NeuralNetwork::NeuralNetwork NetworkList::getNetwork(std::string name) {
+	return networks[getNetworkIndex(name)];
+}
+bool NetworkList::renameNetwork(std::string old_name, std::string new_name) {
+	int index = getNetworkIndex(old_name);
+	if (index == -1) return false;
+	names[index] = new_name;
+	return true;
+}
+int NetworkList::getNetworkIndex(std::string name) {
+	int i = 0;
+	while (i < names.size() && !name._Equal(names[i])) i++;
+	if (i < names.size()) return i;
+	return -1;
+}
+std::vector<std::string> NetworkList::getAllNames() {
+	return names;
+}
---
+++ Machine_Learning_Console/Console/NetworkList.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "NeuralNetworks/NeuralNetwork.h"
+class NetworkList
+{
+	private:
+		std::vector<std::string> names;
+		std::vector<NeuralNetwork::NeuralNetwork> networks;
+		int length;
+		int getNetworkIndex(std::string name);
+
+	public:
+		NetworkList();
+		bool addNetwork(NeuralNetwork::NeuralNetwork network, std::string name);
+		bool removeNetwork(std::string name);
+		void removeAll();
+
+		NeuralNetwork::NeuralNetwork getNetwork(std::string name);
+		bool renameNetwork(std::string old_name, std::string new_name);
+		std::vector<std::string> getAllNames();
+};
+
